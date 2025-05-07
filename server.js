650f8b0a75cb2fc4618688d07174634278a0ee74
Morishima Kureo <jhonsenfranssisco79@gmail.com>
2025-05-07 22:07:01 +0700

    refactor: remove unused http import from server.js
    The `http` module was imported but not used in the server.js file. Removing it improves code cleanliness and reduces unnecessary dependencies.

--- server.js
+++ server.js
@@ -7,8 +7,6 @@ const fs = require('fs');
 const os = require('os');
 const multer = require('multer');
 const upload = multer({ dest: path.join(__dirname, 'server') });
-const http = require('http');
-const socketIo = require('socket.io');
 
 const app = express();
 const server = http.createServer(app);
